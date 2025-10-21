9ebf9b4826c22a1540cc0a619bd9365ce50dce77
bahman <mohammadmahdi.sokhanvar@gmail.com>
2025-10-21 16:22:26 +0330

    chore(webconfig): update CORS configuration for project

--- src/main/java/org/unimanage/config/WebConfig.java
+++ src/main/java/org/unimanage/config/WebConfig.java
@@ -1,5 +1,6 @@
 package org.unimanage.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,6 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+    @Value("${allow.origins}")
+    private String allowedOrigins;
+
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -15,7 +19,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:63342")
+                        .allowedOrigins(allowedOrigins)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
